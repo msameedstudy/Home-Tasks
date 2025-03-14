d6334ab16468065e2b9626af165dc85f12bf5156
msameedstudy <m.sameedstdy@gmail.com>
2025-03-14 11:07:21 -0700

    Add project files.

---
+++ home-task6/q1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    class Product
+    {
+        public int ProductID;
+        public string Name;
+        public double Price;
+
+        public Product(int pid, string name, double price)
+        {
+            Name = name;
+            ProductID = pid;
+            Price = price;
+        }
+
+    }
+
+    class ShoppingCart
+    {
+        private static int cartCounter = 1;
+        public int CartID;
+        public string CustomerName;
+        public double TotalPrice;
+        public Product[] Product = new Product[3];
+        public int productCount;
+
+        public ShoppingCart()
+        {
+            CartID = cartCounter++;
+            CustomerName = "";
+            TotalPrice = 0.0;
+            productCount = 0;
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (productCount < Product.Length)
+            {
+                Product[productCount++] = product;
+                TotalPrice += product.Price;
+                Console.WriteLine("Your Product Has Successfully Added.");
+            }
+            else
+            {
+                Console.WriteLine("Cart is Full Cannot Add More Products.");
+            }
+        }
+
+        public void RemoveProduct(int productID)
+        {
+            for (int i = 0; i < productCount; i++)
+            {
+                if (Product[i] != null && Product[i].ProductID == productID)
+                {
+                    TotalPrice -= Product[i].Price;
+                    Console.WriteLine($"{Product[i].Name} Removed From The Cart.");
+                    for (int j = i; j < productCount - 1; j++)
+                    {
+                        Product[j] = Product[j + 1];
+                    }
+                    Product[productCount - 1] = null;
+                    productCount--;
+                    return;
+                }
+            }
+            Console.WriteLine("Product Not Found");
+        }
+
+
+
+        public void DisplayCart()
+        {
+            Console.WriteLine($"Cart ID: {CartID}, Customer: {CustomerName}");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine("Products in Cart");
+            Console.WriteLine("------------------------------------------");
+            for (int i = 0; i < productCount; i++)
+            {
+                Console.WriteLine($"ID: {Product[i].ProductID}, Name: {Product[i].Name}, Price: {Product[i].Price}");
+            }
+            Console.WriteLine($"Total Price: {TotalPrice}\n");
+           
+        }
+
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingCart cart1 = new ShoppingCart();
+           
+            cart1.CustomerName = "Pappu Pan Wala\n";
+
+            Product p1 = new Product(1, "Katha 500g", 1000);
+            Product p2 = new Product(2, "Chuna 500mg", 14100);
+            Product p3 = new Product(3, "Pan Patta (2kg)", 200);
+
+            cart1.AddProduct(p1);
+            cart1.AddProduct(p2);
+            cart1.AddProduct(p3);
+            Console.WriteLine("------------------------------------------------------------------------");
+            cart1.DisplayCart();
+
+            Console.WriteLine("Enter Product ID for the product you want to remove");
+            int a = Convert.ToInt32(Console.ReadLine());
+            cart1.RemoveProduct(a);
+            cart1.DisplayCart();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ home-task6/q2/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+public class Employee
+{
+    
+    public int EmployeeID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public double Salary { get; set; }
+    public string Department { get; set; }
+
+   
+    public Employee(int employeeID, string fname, string lname, DateTime dob, double salary, string department)
+    {
+        EmployeeID = employeeID;
+        FirstName = fname;
+        LastName = lname;
+        DateOfBirth = dob;
+        Salary = salary;
+        Department = department;
+    }
+}
+
+public class Company
+{
+    public string CompanyName { get; set; }
+    private Employee[] employees;  
+    private int employeeCount; 
+
+    // Constructor
+    public Company(string cname, int count)
+    {
+        CompanyName = cname;
+        employees = new Employee[count]; 
+        employeeCount = 0; 
+    }
+
+   
+    public void AddEmployee(Employee employee)
+    {
+        if (employeeCount < employees.Length)
+        {
+            employees[employeeCount] = employee;
+            employeeCount++; // Increment the count of employees
+            Console.WriteLine($"Employee {employee.FirstName} {employee.LastName} added to the company.");
+        }
+        else
+        {
+            Console.WriteLine("Muft ka paisa nahi ha bhai... No more space to add. Array is full.");
+        }
+    }
+
+    public void RemoveEmployee(int employeeID)
+    {
+        bool found = false;
+
+
+        for (int i = 0; i < employeeCount; i++)
+        {
+            if (employees[i].EmployeeID == employeeID)
+            {
+                found = true;
+                
+                for (int j = i; j < employeeCount - 1; j++)
+                {
+                    employees[j] = employees[j + 1];
+                }
+                
+                employees[employeeCount - 1] = null;
+                employeeCount--; 
+                Console.WriteLine($"Employee with ID {employeeID} removed from the company.");
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Employee not found.");
+        }
+    }
+
+
+    public void displayemp(int employeeID)
+    {
+        bool found = false;
+
+        
+        for (int i = 0; i < employeeCount; i++)
+        {
+            if (employees[i].EmployeeID == employeeID)
+            {
+                found = true;
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine("Employee Details:");
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine($"ID: {employees[i].EmployeeID}");
+                Console.WriteLine($"Name: {employees[i].FirstName} {employees[i].LastName}");
+                Console.WriteLine($"Date of Birth: {employees[i].DateOfBirth.ToShortDateString()}");
+                Console.WriteLine($"Salary: PKR{employees[i].Salary}");
+                Console.WriteLine($"Department: {employees[i].Department}");
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Employee not found.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Company company = new Company("Intech.", 3);
+
+        Employee emp1 = new Employee(101, "Habib", "Shan", new DateTime(1990, 5, 1), 65000, "Coder");
+        Employee emp2 = new Employee(102, "Chand", "Roshan", new DateTime(1985, 8, 12), 75000, "Marketing");
+        Employee emp3 = new Employee(103, "Jan", "dil", new DateTime(1992, 11, 24), 60000, "HR");
+
+        company.AddEmployee(emp1);
+        company.AddEmployee(emp2);
+        company.AddEmployee(emp3);
+        Console.WriteLine("----------------------------------------------------------------");
+
+company.displayemp(101);
+
+       
+        company.RemoveEmployee(102);
+
+
+        company.displayemp(102);
+
+   
+        company.displayemp(101);
+        company.displayemp(103);
+    }
+}
